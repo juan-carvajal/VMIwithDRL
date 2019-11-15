b7ee34a9d81c2dfd9ba0c808cde157adb938cfc3
juan-carvajal <39658930+juan-carvajal@users.noreply.github.com>
2019-11-14 19:38:22 -0500

    Update VMImodel.py

--- VMIwithDRL/src/implementation/VMImodel.py
+++ VMIwithDRL/src/implementation/VMImodel.py
@@ -169,6 +169,7 @@ initial_state = [0, 0, 0, 0, 0,1]
 #print(tensorflow.test.is_gpu_available())
 model = VMI(4, 100, 5, initial_state, 5, 100)
 agent = TrainingAgent(model=model, runs=250, steps_per_run=730, batch_size=32,memory=5000,use_gpu=True)
+agent.run()
 
-agent=QAgent(model,0.9,0.1)
-agent.run(365, 100)
+# agent=QAgent(model,0.9,0.1)
+# agent.run(365, 100)
