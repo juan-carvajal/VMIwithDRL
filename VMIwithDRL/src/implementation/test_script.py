e5a9676d74ccc704f77e5ae866f9dc6a2ac0d73d
juan-carvajal <39658930+juan-carvajal@users.noreply.github.com>
2019-11-17 13:32:12 -0500

    Added stockout and expiree report

--- VMIwithDRL/src/implementation/test_script.py
+++ VMIwithDRL/src/implementation/test_script.py
@@ -8,16 +8,39 @@ from implementation.VMImodel import VMI
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from statistics import mean 
 
 
 initial_state = [0, 0, 0, 0, 0,1]
 #print(tensorflow.test.is_gpu_available())
+train_runs=150
 model = VMI(4, 100, 5, initial_state, 5, 100)
-agent = TrainingAgent(model=model, runs=150, steps_per_run=365, batch_size=32,memory=5000,use_gpu=True,epsilon_function='linear',min_epsilon=0.01,epsilon_min_percentage=0.25)
+agent = TrainingAgent(model=model, runs=train_runs, steps_per_run=365, batch_size=32,memory=5000,use_gpu=True,epsilon_function='linear',min_epsilon=0.01,epsilon_min_percentage=0.25)
 rewards=agent.run()
 log=model.log
-log_df=pd.DataFrame(log)
-print(log_df)
+expirees=[]
+stockouts=[]
+for year in range(train_runs):
+    stk=0
+    exp=0
+    for day in range(len(log[year])):
+        stk+=mean(log[year][day]["stockouts"])
+        exp+=mean(log[year][day]["expirees"])
+    stk=stk/float(len(log[year]))
+    exp=stk/float(len(log[year]))
+    stockouts.append(stk)
+    expirees.append(exp)
+    
+log_data={"stockouts":stockouts,"expirees":expirees}
+#print(log_data)
+log_df=pd.DataFrame(log_data)
+log_df.reset_index(level=0, inplace=True)
+#print(log_df)
+log_df.columns=['index','stockouts','expirees']
+plt.plot(log_df.index, log_df.stockouts,label='Avg. Stockouts')
+plt.plot(log_df.index, log_df.expirees,label='Avg. Expirees',color='orange')
+plt.legend(loc='upper left')
+plt.show()
 
 df=pd.DataFrame(rewards,columns=['rewards'])
 df.reset_index(level=0, inplace=True)
