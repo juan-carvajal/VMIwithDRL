7e3cd4a0ae29861f768db0dfccd3572177dbd95d
juan-carvajal <39658930+juan-carvajal@users.noreply.github.com>
2020-01-09 23:19:41 -0500

    Update

--- VMIwithDRL/src/implementation/hospital.py
+++ VMIwithDRL/src/implementation/hospital.py
@@ -1,4 +1,3 @@
-
 class Hospital():
 
     def __init__(self, ii=None, demand_dist=None, exp_cost=None, stockout_cost=None):
@@ -9,43 +8,34 @@ class Hospital():
 
     def supply(self, supply, demand):
 
-        #inventory_aux = [0] * len(self.inventory)
-        inventory_aux=self.inv_sub_routine(self.inventory,supply,demand)
+        # inventory_aux = [0] * len(self.inventory)
+        inventory_aux = self.inv_sub_routine(self.inventory, supply, demand)
 
         expired = self.inventory[0]
         stockout = max(0, demand - (sum(self.inventory[1:]) + sum(int(v) for v in supply)))
 
-#         for i, val in enumerate(self.inventory):
-#             if i == 0:
-#                 inventory_aux[i] = max(0, self.inventory[i + 1] + supply[i + 1] - demand)
-#             elif 0 < i < 4:
-#                 inventory_aux[i] = max(0, self.inventory[i + 1] + supply[i]
-#                                        - max(0, demand - (
-#                             sum(self.inventory[:i + 1]) + sum(int(v) for v in supply[:i + 1]))))
-#             else:
-#                 inventory_aux[i] = max(0,
-#                                        supply[i] - max(0, demand - (
-#                                                    sum(self.inventory[:i + 1]) + sum(int(v) for v in supply[:i + 1]))))
+        # for i, val in enumerate(self.inventory): if i == 0: inventory_aux[i] = max(0, self.inventory[i + 1] + supply[i + 1]
+        # - demand) elif 0 < i < 4: inventory_aux[i] = max(0, self.inventory[i + 1] + supply[i] - max(0, demand - (
+        # sum(self.inventory[:i + 1]) + sum(int(v) for v in supply[:i + 1])))) else: inventory_aux[i] = max(0,
+        # supply[i] - max(0, demand - ( sum(self.inventory[:i + 1]) + sum(int(v) for v in supply[:i + 1]))))
 
-        self.inventory=inventory_aux
-        #self.stockout.append(stockout)
-        return ((expired * self.exp_cost) + (stockout * self.stockout_cost)),stockout,expired
-    
-    def inv_sub_routine(self,inventory,supply,demand):
+        self.inventory = inventory_aux
+        # self.stockout.append(stockout)
+        return ((expired * self.exp_cost) + (stockout * self.stockout_cost)), stockout, expired
+
+    def inv_sub_routine(self, inventory, supply, demand):
         inventory_aux = [0] * len(inventory)
         for i in range(len(inventory_aux)):
-            if i ==len(inventory_aux)-1:
-                inventory_aux[i]=supply[i]
+            if i == len(inventory_aux) - 1:
+                inventory_aux[i] = supply[i]
             else:
-                inventory_aux[i]=inventory[i+1]+supply[i]
-                
-        
-        d=demand
+                inventory_aux[i] = inventory[i + 1] + supply[i]
+
+        d = demand
         for i in range(len(inventory_aux)):
-            if d >0:
-                rest=inventory_aux[i] if d>inventory_aux[i] else d
-                inventory_aux[i]-=rest
-                d-=rest
-                
+            if d > 0:
+                rest = inventory_aux[i] if d > inventory_aux[i] else d
+                inventory_aux[i] -= rest
+                d -= rest
+
         return inventory_aux
-        
--- VMIwithDRL/src/test_script.py
+++ VMIwithDRL/src/test_script.py
@@ -8,74 +8,88 @@ from implementation.VMImodel import VMI
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from statistics import mean 
+from statistics import mean
 
-
-initial_state = [0, 0, 0, 0, 0,1]
-#print(tensorflow.test.is_gpu_available())
-train_runs=150
+initial_state = [0, 0, 0, 0, 0, 1]
+# print(tensorflow.test.is_gpu_available())
+train_runs = 2000
 model = VMI(4, 100, 5, initial_state, 50, 100)
-agent = TrainingAgent(model=model, runs=train_runs, steps_per_run=365, batch_size=150,memory=5000,use_gpu=False,epsilon_function='linear',min_epsilon=0.01,epsilon_min_percentage=0.2)
-rewards=agent.run()
-log=model.log
-expirees=[]
-stockouts=[]
-dc_expirees=[]
-dataExport=[]
-opt_use=[]
+agent = TrainingAgent(model=model, runs=train_runs, steps_per_run=365, batch_size=150, memory=5000, use_gpu=False,
+                      epsilon_function='linear', min_epsilon=0.01, epsilon_min_percentage=0.2)
+rewards = agent.run()
+log = model.log
+expirees = []
+stockouts = []
+dc_expirees = []
+dataExport = []
+opt_use = []
 for year in range(train_runs):
-    stk=0
-    exp=0
-    dc_exp=0
-    opt=0
+    stk = 0
+    exp = 0
+    dc_exp = 0
+    opt = 0
     for day in range(len(log[year])):
-        stk+=sum(log[year][day]["stockouts"])
-        exp+=sum(log[year][day]["expirees"])
-        dc_exp+=log[year][day]["DC_expirees"]
-        #all=[list(x) for x in zip(*log[year][day]["allocation"])]
+        stk += sum(log[year][day]["stockouts"])
+        exp += sum(log[year][day]["expirees"])
+        dc_exp += log[year][day]["DC_expirees"]
+        # all=[list(x) for x in zip(*log[year][day]["allocation"])]
         if log[year][day]['Used_LP_Model']:
-            opt+=1
-            
-        all=log[year][day]["allocation"]
-        a=[year,day,log[year][day]["action"]]+log[year][day]["inventory"]+[log[year][day]["reward"]]+log[year][day]["demands"]+[log[year][day]["donors"]]+log[year][day]["stockouts"]+log[year][day]["expirees"]+[log[year][day]["DC_expirees"]]+[item for sublist in all for item in sublist]+[item for sublist in log[year][day]["II"] for item in sublist]
+            opt += 1
+
+        all = log[year][day]["allocation"]
+        a = [year, day, log[year][day]["action"]] + log[year][day]["inventory"] + [log[year][day]["reward"]] + \
+            log[year][day]["demands"] + [log[year][day]["donors"]] + log[year][day]["stockouts"] + log[year][day][
+                "expirees"] + [log[year][day]["DC_expirees"]] + [item for sublist in all for item in sublist] + [item
+                                                                                                                 for
+                                                                                                                 sublist
+                                                                                                                 in log[
+                                                                                                                     year][
+                                                                                                                     day][
+                                                                                                                     "II"]
+                                                                                                                 for
+                                                                                                                 item in
+                                                                                                                 sublist]
         dataExport.append(a)
-        
-    opt_use.append(opt/len(log[year]))
+
+    opt_use.append(opt / len(log[year]))
     stockouts.append(stk)
     expirees.append(exp)
     dc_expirees.append(dc_exp)
-    
-log_export=pd.DataFrame(dataExport)
+
+log_export = pd.DataFrame(dataExport)
 log_export.reset_index(level=0, inplace=True)
-log_export.columns=['index','year','day','action','I0','I1','I2','I3','I4','reward','D1','D2','D3','D4','donors','S1','S2','S3','S4','E1','E2','E3','E4','DC_E','H1_A0','H1_A1','H1_A2','H1_A3','H1_A4','H2_A0','H2_A1','H2_A2','H2_A3','H2_A4','H3_A0','H3_A1','H3_A2','H3_A3','H3_A4','H4_A0','H4_A1','H4_A2','H4_A3','H4_A4','H1_II0','H1_II1','H1_II2','H1_II3','H1_II4','H2_II0','H2_II1','H2_II2','H2_II3','H2_II4','H3_II0','H3_II1','H3_II2','H3_II3','H3_II4','H4_II0','H4_II1','H4_II2','H4_II3','H4_II4']
+log_export.columns = ['index', 'year', 'day', 'action', 'I0', 'I1', 'I2', 'I3', 'I4', 'reward', 'D1', 'D2', 'D3', 'D4',
+                      'donors', 'S1', 'S2', 'S3', 'S4', 'E1', 'E2', 'E3', 'E4', 'DC_E', 'H1_A0', 'H1_A1', 'H1_A2',
+                      'H1_A3', 'H1_A4', 'H2_A0', 'H2_A1', 'H2_A2', 'H2_A3', 'H2_A4', 'H3_A0', 'H3_A1', 'H3_A2', 'H3_A3',
+                      'H3_A4', 'H4_A0', 'H4_A1', 'H4_A2', 'H4_A3', 'H4_A4', 'H1_II0', 'H1_II1', 'H1_II2', 'H1_II3',
+                      'H1_II4', 'H2_II0', 'H2_II1', 'H2_II2', 'H2_II3', 'H2_II4', 'H3_II0', 'H3_II1', 'H3_II2',
+                      'H3_II3', 'H3_II4', 'H4_II0', 'H4_II1', 'H4_II2', 'H4_II3', 'H4_II4']
 log_export.to_csv('data.csv')
-    
-log_data={"stockouts":stockouts,"expirees":expirees,"dc_expirees":dc_expirees}
-#print(log_data)
-log_df=pd.DataFrame(log_data)
+
+log_data = {"stockouts": stockouts, "expirees": expirees, "dc_expirees": dc_expirees}
+# print(log_data)
+log_df = pd.DataFrame(log_data)
 log_df.reset_index(level=0, inplace=True)
-#print(log_df)
-log_df.columns=['index','stockouts','expirees','dc_expirees']
-plt.plot(log_df.index, log_df.stockouts,label='Stockouts')
-plt.plot(log_df.index, log_df.expirees,label='Expirees',color='orange')
-plt.plot(log_df.index, log_df.dc_expirees,label='DC Expirees',color='green')
+# print(log_df)
+log_df.columns = ['index', 'stockouts', 'expirees', 'dc_expirees']
+plt.plot(log_df.index, log_df.stockouts, label='Stockouts')
+plt.plot(log_df.index, log_df.expirees, label='Expirees', color='orange')
+plt.plot(log_df.index, log_df.dc_expirees, label='DC Expirees', color='green')
 plt.legend(loc='upper left')
 plt.show()
 
-
-
-df=pd.DataFrame(rewards,columns=['rewards'])
+df = pd.DataFrame(rewards, columns=['rewards'])
 df.reset_index(level=0, inplace=True)
-df.columns=['index','data']
+df.columns = ['index', 'data']
 rolling_mean = df.data.rolling(window=50).mean()
 plt.plot(df.index, df.data, label='Rewards')
 plt.plot(df.index, rolling_mean, label='SMA(n=50)', color='orange')
 plt.legend(loc='upper left')
 plt.show()
 
-opt_df=pd.DataFrame(opt_use,columns=['opt'])
+opt_df = pd.DataFrame(opt_use, columns=['opt'])
 opt_df.reset_index(level=0, inplace=True)
-opt_df.columns=['index','opt']
+opt_df.columns = ['index', 'opt']
 plt.plot(opt_df.index, opt_df.opt, label='Opt. Model Use')
 plt.legend(loc='upper left')
 plt.show()
