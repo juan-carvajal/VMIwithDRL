d274d56638c99e37d6b06e761994e43e49e5af2f
juan-carvajal <39658930+juan-carvajal@users.noreply.github.com>
2020-01-14 13:23:59 -0500

    Update a few method in the state transition phase

--- VMIwithDRL/src/implementation/hospital.py
+++ VMIwithDRL/src/implementation/hospital.py
@@ -9,10 +9,26 @@ class Hospital():
     def supply(self, supply, demand):
 
         # inventory_aux = [0] * len(self.inventory)
-        inventory_aux = self.inv_sub_routine(self.inventory, supply, demand)
+        #inventory_aux = self.inv_sub_routine(self.inventory, supply, demand)
+        inventory_aux=[ self.inventory[i]+supply[i] for i in range(len(self.inventory))]
+        d = demand
+        for i in range(len(inventory_aux)):
+            if d > 0:
+                rest = inventory_aux[i] if d > inventory_aux[i] else d
+                inventory_aux[i] -= rest
+                d -= rest
 
-        expired = self.inventory[0]
+        expired = inventory_aux[0]
         stockout = max(0, demand - (sum(self.inventory[1:]) + sum(int(v) for v in supply)))
+
+        for i in range(len(inventory_aux)):
+            if i==len(inventory_aux)-1:
+                inventory_aux[i]=0
+            else:
+                inventory_aux[i]=inventory_aux[i+1]
+
+        # expired = max(0, self.inventory[0] + supply[0] - demand)
+        # stockout = max(0, demand - (sum(self.inventory[1:]) + sum(int(v) for v in supply)))
 
         # for i, val in enumerate(self.inventory): if i == 0: inventory_aux[i] = max(0, self.inventory[i + 1] + supply[i + 1]
         # - demand) elif 0 < i < 4: inventory_aux[i] = max(0, self.inventory[i + 1] + supply[i] - max(0, demand - (
--- VMIwithDRL/src/test_script.py
+++ VMIwithDRL/src/test_script.py
@@ -12,9 +12,9 @@ from statistics import mean
 if __name__ == '__main__':
     initial_state = [0, 0, 0, 0, 0, 1]
     # print(tensorflow.test.is_gpu_available())
-    train_runs = 200
+    train_runs = 1000
     model = VMI(4, 100, 5, initial_state, 5, 100)
-    agent = TrainingAgent(model=model, runs=train_runs, steps_per_run=365, batch_size=150, memory=5000, use_gpu=False,
+    agent = TrainingAgent(model=model, runs=train_runs, steps_per_run=365, batch_size=32, memory=5000, use_gpu=False,
                           epsilon_function='linear', min_epsilon=0.01, epsilon_min_percentage=0.2)
     rewards = agent.run()
     log = model.log
