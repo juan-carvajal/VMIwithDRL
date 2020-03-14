bf1eadcf09e6d9e54f4ca8f9de4ae13be1dc51a5
juan-carvajal <39658930+juan-carvajal@users.noreply.github.com>
2020-03-14 18:50:03 -0500

    Setup for 2000 runs

--- VMIwithDRL/src/test_script.py
+++ VMIwithDRL/src/test_script.py
@@ -59,12 +59,12 @@ if __name__ == '__main__':
 
     # agent = TrainingAgent(model=model, runs=train_runs, steps_per_run=365, batch_size=32, memory=10000, use_gpu=True,
     #                       epsilon_function='linear', min_epsilon=0.005, epsilon_min_percentage=0.2)
-    train_runs = 50
+    train_runs = 2000
     model = VMI(4, 100, 5, train_runs, initial_state, 5, 100)
-    agent = TrainingAgent(model=model, runs=train_runs, steps_per_run=365, batch_size=10, memory=100000, use_gpu=True,
-                          epsilon_function='logv2', min_epsilon=0.005, epsilon_min_percentage=0.1)
+    agent = TrainingAgent(model=model, runs=train_runs, steps_per_run=365, batch_size=10, memory=127750, use_gpu=True,
+                          epsilon_function='logv2', min_epsilon=0.01, epsilon_min_percentage=0.1)
     rewards = agent.run()
-    validate_runs = 10
+    validate_runs = 500
     val_rewards = agent.validate(validate_runs, 365)
     print("Total state space: ", [len(x) for x in model.state_space_memory])
     log = model.log["train"]
